014aa494ce6845c82377212a4b425d4b50ef6d0f
AngelGarcia1019 <18161117@itoaxaca.edu.mx>
2024-03-07 21:25:00 -0600

    Add: solutions comments

--- 01.js
+++ 01.js
@@ -1,3 +1,8 @@
+// 1.- Escribe una función que dados dos árboles binarios A y B, determine si son idénticos o no.
+// 2.- Escribe una función que dado un árbol binario A, obtenga una copia B del mismo.
+// 3.- Escribe una función que visualice los nodos que están en el nivel n de un árbol binario.
+// 4.- Escribe una función que devuelva el número de hojas de un árbol binario.
+
 const BinaryTree = require("./tree");
 
 const a = new BinaryTree();
@@ -17,12 +22,16 @@ b.add(6);
 
 b.print();
 
+// 1.- Escribe una función que dados dos árboles binarios A y B, determine si son idénticos o no.
 console.log(`Son identicos: ${a.isIdenticalTree(b) ? "Si" : "No"}`);
 
+// 2.- Escribe una función que dado un árbol binario A, obtenga una copia B del mismo.
 console.log(`Copia: `);
 const treeCopyA = a.copyTree();
 treeCopyA.print();
 
+// 3.- Escribe una función que visualice los nodos que están en el nivel n de un árbol binario.
 a.printNodesAtLevelData(3);
 
+// 4.- Escribe una función que devuelva el número de hojas de un árbol binario.
 console.log(`Hoas en el arbol a: ${a.countLeavesInTree()}`);
